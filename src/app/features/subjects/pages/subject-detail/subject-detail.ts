cc5693f8ef7da1011945ad92aa80c115779bb781
Avinash Batule <avinash.batule1994@gmail.com>
2026-07-27 17:32:21 +0530

    artictecuture is completed

---
+++ src/app/features/subjects/pages/subject-detail/subject-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subject-detail',
+  imports: [],
+  templateUrl: './subject-detail.html',
+  styleUrl: './subject-detail.scss',
+})
+export class SubjectDetail {}
---
+++ src/app/features/subjects/pages/topic-detail/topic-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-detail',
+  imports: [],
+  templateUrl: './topic-detail.html',
+  styleUrl: './topic-detail.scss',
+})
+export class TopicDetail {}
